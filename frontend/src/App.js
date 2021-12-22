090c01b2800888790d847040f3b26a2587873f52
Levente26 <madi.levente26@gmail.com>
2021-12-22 02:14:23 +0100

    frontend reach datas & images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+import { resturants } from './resturants';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+     {resturants.map(resturant=><p>{resturant.name}</p>)}
     </div>
   );
 }
